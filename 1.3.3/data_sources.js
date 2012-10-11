9fd89ed0c4284876db7517ee80acfa4623929218
Michael Cahill <michael.cahill@wiredtiger.com>
2012-10-11 13:38:07 +1100

    Documentation update for 1.3.3

---
+++ 1.3.3/data_sources.js
@@ -0,0 +1,16 @@
+var data_sources =
+[
+    [ "Builtin data sources", "data_sources.html#data_builtin", [
+      [ "Hot backup cursors", "data_sources.html#data_backup", null ],
+      [ "Raw Files", "data_sources.html#data_files", null ],
+      [ "Table Index data", "data_sources.html#data_indices", null ],
+      [ "Statistics Data", "data_sources.html#data_statistics", null ]
+    ] ],
+    [ "Cursor operations", "cursor_ops.html", [
+      [ "Opening a cursor", "cursor_ops.html#cursor_opening", null ],
+      [ "Closing a cursor", "cursor_ops.html#cursor_closing", null ],
+      [ "Positioning a cursor", "cursor_ops.html#cursor_position", null ],
+      [ "Inserting and updating", "cursor_ops.html#cursor_writes", null ],
+      [ "Cursor position after error", "cursor_ops.html#cursor_error", null ]
+    ] ]
+];
