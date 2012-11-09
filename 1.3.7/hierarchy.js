c268dfedbc05eb1150d881d30e8794871ea1f7af
Michael Cahill <michael.cahill@wiredtiger.com>
2012-11-09 23:05:09 +1100

    Docs for 1.3.7

---
+++ 1.3.7/hierarchy.js
@@ -0,0 +1,20 @@
+var hierarchy =
+[
+    [ "object", null, [
+      [ "wiredtiger.Connection", "classwiredtiger_1_1_connection.html", null ],
+      [ "wiredtiger.Cursor", "classwiredtiger_1_1_cursor.html", null ],
+      [ "wiredtiger.Session", "classwiredtiger_1_1_session.html", null ]
+    ] ],
+    [ "wiredtiger.filestat", "classwiredtiger_1_1filestat.html", null ],
+    [ "wiredtiger.stat", "classwiredtiger_1_1stat.html", null ],
+    [ "WT_COLLATOR", "struct_w_t___c_o_l_l_a_t_o_r.html", null ],
+    [ "WT_COMPRESSOR", "struct_w_t___c_o_m_p_r_e_s_s_o_r.html", null ],
+    [ "WT_CONNECTION", "struct_w_t___c_o_n_n_e_c_t_i_o_n.html", null ],
+    [ "WT_CURSOR", "struct_w_t___c_u_r_s_o_r.html", null ],
+    [ "WT_DATA_SOURCE", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html", null ],
+    [ "WT_EVENT_HANDLER", "struct_w_t___e_v_e_n_t___h_a_n_d_l_e_r.html", null ],
+    [ "WT_EXTENSION_API", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html", null ],
+    [ "WT_EXTRACTOR", "struct_w_t___e_x_t_r_a_c_t_o_r.html", null ],
+    [ "WT_ITEM", "group__wt.html#struct_w_t___i_t_e_m", null ],
+    [ "WT_SESSION", "struct_w_t___s_e_s_s_i_o_n.html", null ]
+];
