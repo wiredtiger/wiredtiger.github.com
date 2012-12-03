a1743e3270c361727440c3b69da07d4ef90c8059
Michael Cahill <michael.cahill@wiredtiger.com>
2012-12-03 21:47:00 +1100

    1.4.0 docs

---
+++ 1.4.0/data_sources.js
@@ -0,0 +1,15 @@
+var data_sources =
+[
+    [ "Builtin data sources", "data_sources.html#data_builtin", [
+      [ "Raw Files", "data_sources.html#data_files", null ],
+      [ "Table Index data", "data_sources.html#data_indices", null ],
+      [ "Statistics Data", "data_sources.html#data_statistics", null ]
+    ] ],
+    [ "Cursor operations", "cursor_ops.html", [
+      [ "Opening a cursor", "cursor_ops.html#cursor_opening", null ],
+      [ "Closing a cursor", "cursor_ops.html#cursor_closing", null ],
+      [ "Positioning a cursor", "cursor_ops.html#cursor_position", null ],
+      [ "Inserting and updating", "cursor_ops.html#cursor_writes", null ],
+      [ "Cursor position after error", "cursor_ops.html#cursor_error", null ]
+    ] ]
+];
---
+++ 1.4.0/group__wt__ext.js
@@ -0,0 +1,29 @@
+var group__wt__ext =
+[
+    [ "WT_COLLATOR", "struct_w_t___c_o_l_l_a_t_o_r.html", [
+      [ "compare", "struct_w_t___c_o_l_l_a_t_o_r.html#a9a6de866802dcd1a587cb1dbd69887df", null ]
+    ] ],
+    [ "WT_COMPRESSOR", "struct_w_t___c_o_m_p_r_e_s_s_o_r.html", [
+      [ "compress", "struct_w_t___c_o_m_p_r_e_s_s_o_r.html#a87c339a3442b719896fa5cbe2c530843", null ],
+      [ "compress_raw", "struct_w_t___c_o_m_p_r_e_s_s_o_r.html#a9f1ce6dca12efba27b6a5ce02c473d39", null ],
+      [ "decompress", "struct_w_t___c_o_m_p_r_e_s_s_o_r.html#acf978486348484d467bd4f4032cf3795", null ],
+      [ "pre_size", "struct_w_t___c_o_m_p_r_e_s_s_o_r.html#a0ae8cd41e9f3d65187a5e0209a075f02", null ]
+    ] ],
+    [ "WT_DATA_SOURCE", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html", [
+      [ "create", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html#a214f53ea79c931550a6b51de3e3aaa94", null ],
+      [ "drop", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html#ac5f93f2d017533b3fb0238f4e55940da", null ],
+      [ "open_cursor", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html#a73eddca0e95e7369381ffdb6ee42682b", null ],
+      [ "rename", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html#ad534774631e56f855999043c2f0d0677", null ],
+      [ "truncate", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html#ab19f5230a8282cdaa8159f79c94e7508", null ]
+    ] ],
+    [ "WT_EXTRACTOR", "struct_w_t___e_x_t_r_a_c_t_o_r.html", [
+      [ "extract", "struct_w_t___e_x_t_r_a_c_t_o_r.html#a7a7069beebb84ffaa8ee02f8d7ebb162", null ]
+    ] ],
+    [ "WT_EXTENSION_API", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html", [
+      [ "err_printf", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a4bab25a9672a9bb8d7912a1180cf4fc2", null ],
+      [ "scr_alloc", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a5c3097b7329cecbf73db8db42ed3ad68", null ],
+      [ "scr_free", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a6a52d691577692271f2566aa6fc5f10e", null ]
+    ] ],
+    [ "wiredtiger_extension_init", "group__wt__ext.html#gaff9d80473c3e08cc7a04a69e690d8cd7", null ],
+    [ "wt_api", "group__wt__ext.html#gaf24317169f39f574d889a1868f83510a", null ]
+];
