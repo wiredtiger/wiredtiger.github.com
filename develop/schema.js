cab36e81d4ef319bd1916777c2452a1a65efc503
Michael Cahill <mjc@plenty.home.oz.au>
2012-12-09 10:00:07 +1100

    Update auto-generated develop docs

--- develop/schema.js
+++ develop/schema.js
@@ -1,12 +1,13 @@
 var schema =
 [
     [ "Tables, rows and columns", "schema.html#schema_intro", null ],
-    [ "Column types", "schema.html#schema_types", null ],
+    [ "Column types", "schema.html#schema_column_types", null ],
     [ "Format types", "schema.html#schema_format_types", null ],
-    [ "Columns in key and values", "schema.html#schema_data_access", null ],
-    [ "Describing columns", "schema.html#schema_columns", null ],
-    [ "Storing groups of columns together", "schema.html#schema_column_groups", null ],
-    [ "Adding an index", "schema.html#schema_indices", null ],
+    [ "Key and value formats", "schema.html#schema_key_and_value_formats", null ],
+    [ "Cursor formats", "schema.html#schema_cursor_formats", null ],
+    [ "Columns", "schema.html#schema_columns", null ],
+    [ "Column groups", "schema.html#schema_column_groups", null ],
+    [ "Column indices", "schema.html#schema_column_indices", null ],
     [ "Code samples", "schema.html#schema_examples", null ],
     [ "Key/Value pairs", "keyvalue.html", null ],
     [ "Packing and Unpacking Data", "packing.html", [
