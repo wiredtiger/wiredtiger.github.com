ce22e658582a202db4b565005c4aa23027a1a782
Michael Cahill <mjc@plenty.home.oz.au>
2013-03-30 08:00:06 +1100

    Update auto-generated develop docs

--- develop/struct_w_t___d_a_t_a___s_o_u_r_c_e.js
+++ develop/struct_w_t___d_a_t_a___s_o_u_r_c_e.js
@@ -2,7 +2,7 @@ var struct_w_t___d_a_t_a___s_o_u_r_c_e =
 [
     [ "create", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html#a214f53ea79c931550a6b51de3e3aaa94", null ],
     [ "drop", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html#ac5f93f2d017533b3fb0238f4e55940da", null ],
-    [ "open_cursor", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html#a73eddca0e95e7369381ffdb6ee42682b", null ],
+    [ "open_cursor", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html#af34269487e490e280344687be36a24b0", null ],
     [ "rename", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html#ad534774631e56f855999043c2f0d0677", null ],
     [ "truncate", "struct_w_t___d_a_t_a___s_o_u_r_c_e.html#ab19f5230a8282cdaa8159f79c94e7508", null ]
 ];
