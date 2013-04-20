37be9c5051e6dc3baaedee59970ae59436dbde54
Michael Cahill <mjc@plenty.home.oz.au>
2013-04-21 02:00:06 +1000

    Update auto-generated develop docs

--- develop/group__wt__ext.js
+++ develop/group__wt__ext.js
@@ -30,7 +30,8 @@ var group__wt__ext =
       [ "err_printf", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a83a12d3bf85f508b2f85c76ca2e9a039", null ],
       [ "msg_printf", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a460e65aa13990254a2cd83fe96d4865a", null ],
       [ "scr_alloc", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a72da81054ac80fc95226a98ef094df2f", null ],
-      [ "scr_free", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#aba236f198ae9227793fff13cff1ba159", null ]
+      [ "scr_free", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#aba236f198ae9227793fff13cff1ba159", null ],
+      [ "strerror", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a6218dcf11a0129f0c3a21abc04157e5f", null ]
     ] ],
     [ "WT_CONFIG_ITEM", "struct_w_t___c_o_n_f_i_g___i_t_e_m.html", [
       [ "WT_CONFIG_ITEM_STRING", "struct_w_t___c_o_n_f_i_g___i_t_e_m.html#a215d5ce41e53aa467dd940bace3f25f0aaf3e84e05814a538b630d161edba9c2d", null ],
--- develop/struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.js
+++ develop/struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.js
@@ -7,5 +7,6 @@ var struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i =
     [ "err_printf", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a83a12d3bf85f508b2f85c76ca2e9a039", null ],
     [ "msg_printf", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a460e65aa13990254a2cd83fe96d4865a", null ],
     [ "scr_alloc", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a72da81054ac80fc95226a98ef094df2f", null ],
-    [ "scr_free", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#aba236f198ae9227793fff13cff1ba159", null ]
+    [ "scr_free", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#aba236f198ae9227793fff13cff1ba159", null ],
+    [ "strerror", "struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a6218dcf11a0129f0c3a21abc04157e5f", null ]
 ];
