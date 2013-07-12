99933f807a6fa592347a260819df8ae05eeba67c
Alex Gorrod <alexg@wiredtiger.com>
2013-07-12 13:34:04 +1000

    Release 1.6.3

---
+++ 1.6.3/classwiredtiger_1_1_session.js
@@ -0,0 +1,20 @@
+var classwiredtiger_1_1_session =
+[
+    [ "__init__", "classwiredtiger_1_1_session.html#aed00603cc6df8c4b75585003157c1ffd", null ],
+    [ "begin_transaction", "classwiredtiger_1_1_session.html#ac041afc6a582ea2a9dd9af60a0603fdd", null ],
+    [ "checkpoint", "classwiredtiger_1_1_session.html#a7a8b13b96530447f26a07e7d85521659", null ],
+    [ "close", "classwiredtiger_1_1_session.html#a63f702c75007d8d25165ebf55ee92739", null ],
+    [ "commit_transaction", "classwiredtiger_1_1_session.html#aea2d198c1ce6615ac2d5fecccd1be625", null ],
+    [ "compact", "classwiredtiger_1_1_session.html#afbf37a8455a785f7aabbfc29e91cde81", null ],
+    [ "create", "classwiredtiger_1_1_session.html#a05806032431c2420bf709d7cfdb6937c", null ],
+    [ "drop", "classwiredtiger_1_1_session.html#aa5636db260be86b90228978057daea34", null ],
+    [ "open_cursor", "classwiredtiger_1_1_session.html#aa7f8ff75031b4ee2f337cf2776361a79", null ],
+    [ "reconfigure", "classwiredtiger_1_1_session.html#aa761c6e0840b09436289b4a7db0befc2", null ],
+    [ "rename", "classwiredtiger_1_1_session.html#a661a9bb6dcaf9d6484758ec81d8772b9", null ],
+    [ "rollback_transaction", "classwiredtiger_1_1_session.html#a56d07de3ee9db0059fe25f160c9f9cd3", null ],
+    [ "salvage", "classwiredtiger_1_1_session.html#ac77449102799219855d198197426af5e", null ],
+    [ "truncate", "classwiredtiger_1_1_session.html#a80a9ee8697a61a1ad13d893d67d981bb", null ],
+    [ "upgrade", "classwiredtiger_1_1_session.html#a10d14f378fa6368b0f5d1923a3473188", null ],
+    [ "verify", "classwiredtiger_1_1_session.html#a30e548e0d6121f50de5b401f76ccdc39", null ],
+    [ "this", "classwiredtiger_1_1_session.html#ad3453e7cb4601a5c0c6e512fa5bffe3b", null ]
+];
