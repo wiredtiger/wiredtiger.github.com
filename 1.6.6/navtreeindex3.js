63b7c66afa718fbf0a172de651922d40603f5aec
Alex Gorrod <alexg@wiredtiger.com>
2013-11-19 11:54:42 +1100

    Release 1.6.6

---
+++ 1.6.6/navtreeindex3.js
@@ -0,0 +1,10 @@
+var NAVTREEINDEX3 =
+{
+"upgrading.html#version_160":[0,4,6],
+"upgrading.html#version_161":[0,4,5],
+"upgrading.html#version_162":[0,4,4],
+"upgrading.html#version_163":[0,4,3],
+"upgrading.html#version_164":[0,4,2],
+"upgrading.html#version_165":[0,4,1],
+"upgrading.html#version_166":[0,4,0]
+};
