b93b6bc330b5c7705d25cb30ba44743e6daf5c3f
Michael Cahill <michael.cahill@mongodb.com>
2015-08-27 11:00:10 +1000

    Update auto-generated develop docs

--- develop/navtreeindex5.js
+++ develop/navtreeindex5.js
@@ -1,9 +1,9 @@
 var NAVTREEINDEX5 =
 {
-"wtperf.html#monitor":[0,5,22,0],
 "wtperf.html#monitor":[0,4,28,0],
-"wtstats.html":[0,5,23],
+"wtperf.html#monitor":[0,5,22,0],
 "wtstats.html":[0,4,29],
+"wtstats.html":[0,5,23],
 "wtstats.html#wtstats_display":[0,5,23,0],
 "wtstats.html#wtstats_display":[0,4,29,0]
 };
