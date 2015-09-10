98b00ec185069cfd74bc0730cb0a15c44024a336
Michael Cahill <michael.cahill@mongodb.com>
2015-09-10 11:00:08 +1000

    Update auto-generated develop docs

--- develop/navtreedata.js
+++ develop/navtreedata.js
@@ -22,9 +22,9 @@ var NAVTREEINDEX =
 ".html",
 "classcom_1_1wiredtiger_1_1db_1_1_pack_output_stream.html#a2bda605ed3c4c4b85a52e145ea809f61",
 "examples.html",
-"group__wt.html#ga8ecf265dba023ad13a6cdf3e02fa9d50",
-"struct_w_t___a_s_y_n_c___o_p.html#a3c0ab580d7d7cc18f669ef8259254b8c",
-"upgrading.html#version_250"
+"group__wt.html#ga8e02ca5d30b1fa3f8a7dba281ae2633f",
+"struct_w_t___a_s_y_n_c___o_p.html#a46ec0ae7d0294b9f4f206c390dd886b3",
+"upgrading.html#version_251"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- develop/navtreeindex5.js
+++ develop/navtreeindex5.js
@@ -1,6 +1,5 @@
 var NAVTREEINDEX5 =
 {
-"upgrading.html#version_250":[0,9,6],
 "upgrading.html#version_251":[0,9,5],
 "upgrading.html#version_252":[0,9,4],
 "upgrading.html#version_253":[0,9,3],
@@ -11,10 +10,10 @@ var NAVTREEINDEX5 =
 "wtperf.html":[0,4,28],
 "wtperf.html#config":[0,5,22,1],
 "wtperf.html#config":[0,4,28,1],
-"wtperf.html#monitor":[0,5,22,0],
 "wtperf.html#monitor":[0,4,28,0],
-"wtstats.html":[0,5,23],
+"wtperf.html#monitor":[0,5,22,0],
 "wtstats.html":[0,4,29],
+"wtstats.html":[0,5,23],
 "wtstats.html#wtstats_display":[0,5,23,0],
 "wtstats.html#wtstats_display":[0,4,29,0]
 };
