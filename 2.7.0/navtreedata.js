10651f4c06e0bbacac5796072f91f8b474802eea
Michael Cahill <michael.cahill@mongodb.com>
2015-11-19 17:14:58 +1100

    Release 2.7.0

---
+++ 2.7.0/navtreedata.js
@@ -0,0 +1,31 @@
+var NAVTREE =
+[
+  [ "WiredTiger", "index.html", [
+    [ "Reference Guide", "index.html", "index" ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Class List", "annotated.html", "annotated" ],
+    [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+    [ "Class Members", "functions.html", [
+      [ "All", "functions.html", "functions_dup" ],
+      [ "Functions", "functions_func.html", "functions_func" ],
+      [ "Variables", "functions_vars.html", null ],
+      [ "Enumerator", "functions_eval.html", null ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ],
+    [ "Community", "community.html", null ],
+    [ "License", "license.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classcom_1_1wiredtiger_1_1db_1_1_pack_output_stream.html#a2bda605ed3c4c4b85a52e145ea809f61",
+"ex_pack_8c-example.html",
+"group__wt.html#ga7fa6ef6ad4ec854de76565ebce34cede",
+"schema_lang_java.html#schema_examples_lang_java",
+"upgrading.html#version_13"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
