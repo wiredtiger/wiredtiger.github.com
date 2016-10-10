3c6b491ce8d174eebbfa27aed602d8d7985ba957
Michael Cahill <michael.cahill@mongodb.com>
2016-10-10 16:11:04 +1100

    Update auto-generated develop docs

---
+++ mongodb-3.2/navtreedata.js
@@ -0,0 +1,31 @@
+var NAVTREE =
+[
+  [ "WiredTiger", "index.html", [
+    [ "Reference Guide", "index.html", "index" ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Class List", "annotated.html", "annotated" ],
+    [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+    [ "Class Members", "functions.html", [
+      [ "All", "functions.html", "functions_dup" ],
+      [ "Functions", "functions_func.html", "functions_func" ],
+      [ "Variables", "functions_vars.html", null ],
+      [ "Enumerator", "functions_eval.html", null ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ],
+    [ "Community", "community.html", null ],
+    [ "License", "license.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classcom_1_1wiredtiger_1_1db_1_1_pack_input_stream.html#aec0dfa7f50233e7aea607ac2f1c2bb89",
+"ex_call_center_8c-example.html",
+"group__wt.html#ga696e6723bc1e3d4c3e5b5dde84fed73e",
+"in_memory.html",
+"struct_w_t___f_i_l_e___h_a_n_d_l_e.html#aadcd969865c263c9e9b00d39308e296c"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ mongodb-3.4/navtreedata.js
@@ -0,0 +1,31 @@
+var NAVTREE =
+[
+  [ "WiredTiger", "index.html", [
+    [ "Reference Guide", "index.html", "index" ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Class List", "annotated.html", "annotated" ],
+    [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+    [ "Class Members", "functions.html", [
+      [ "All", "functions.html", "functions_dup" ],
+      [ "Functions", "functions_func.html", "functions_func" ],
+      [ "Variables", "functions_vars.html", null ],
+      [ "Enumerator", "functions_eval.html", null ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ],
+    [ "Community", "community.html", null ],
+    [ "License", "license.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classcom_1_1wiredtiger_1_1db_1_1_pack_input_stream.html#aec0dfa7f50233e7aea607ac2f1c2bb89",
+"ex_call_center_8c-example.html",
+"group__wt.html#ga66c77276624f1d35095653b79af42326",
+"home.html",
+"struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a8a8cadc351799d3f5c1b55d266cff780"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
