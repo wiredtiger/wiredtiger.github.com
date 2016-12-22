650236fc4efc72c4888ee91e1d3ee8329791c6a1
David Hows <howsdav@gmail.com>
2016-12-23 10:44:35 +1100

    Release 2.9.1

---
+++ 2.9.1/navtreedata.js
@@ -0,0 +1,31 @@
+var NAVTREE =
+[
+  [ "WiredTiger", "index.html", [
+    [ "Reference Guide", "index.html", "index" ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Class List", "annotated.html", "annotated" ],
+    [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+    [ "Class Members", "functions.html", [
+      [ "All", "functions.html", "functions_dup" ],
+      [ "Functions", "functions_func.html", "functions_func" ],
+      [ "Variables", "functions_vars.html", null ],
+      [ "Enumerator", "functions_eval.html", null ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ],
+    [ "Community", "community.html", null ],
+    [ "License", "license.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classcom_1_1wiredtiger_1_1db_1_1_pack_input_stream.html#acfc4ad09cf8faf58536523169512c94a",
+"enumcom_1_1wiredtiger_1_1db_1_1_search_status.html#aa6979e5adfd45aaedf3a893652a527e7",
+"group__wt.html#ga58af084ac45e0367be4492c50ffaadce",
+"group__wt.html#gafa8782397609778ea3efe002f85cb178",
+"struct_w_t___e_n_c_r_y_p_t_o_r.html#ad9337c89e540be05d207995c33bbe993"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
