02229eeb58fcfa1d21db2ffd9b749650aac34cb7
Michael Cahill <michael.cahill@mongodb.com>
2017-04-06 02:00:05 +1000

    Update auto-generated develop docs

--- develop/navtreeindex6.js
+++ develop/navtreeindex6.js
@@ -1,4 +1,7 @@
 var NAVTREEINDEX6 =
 {
-"wtstats.html#wtstats_display":[0,5,27,0]
+"wtstats.html":[0,4,34],
+"wtstats.html":[0,5,27],
+"wtstats.html#wtstats_display":[0,5,27,0],
+"wtstats.html#wtstats_display":[0,4,34,0]
 };
