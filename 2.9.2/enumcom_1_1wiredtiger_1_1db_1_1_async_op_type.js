64ded0f5f592b087ebc258b3fdc082bf5bfa3fef
Alex Gorrod <alexander.gorrod@mongodb.com>
2017-05-27 01:34:46 +1000

    Release 2.9.2

---
+++ 2.9.2/enumcom_1_1wiredtiger_1_1db_1_1_async_op_type.js
@@ -0,0 +1,9 @@
+var enumcom_1_1wiredtiger_1_1db_1_1_async_op_type =
+[
+    [ "swigValue", "enumcom_1_1wiredtiger_1_1db_1_1_async_op_type.html#a1234aee7a116e269ecaa5cfc102f4ae2", null ],
+    [ "WT_AOP_COMPACT", "enumcom_1_1wiredtiger_1_1db_1_1_async_op_type.html#a99e6a79325373f6568a365f845ecd8b1", null ],
+    [ "WT_AOP_INSERT", "enumcom_1_1wiredtiger_1_1db_1_1_async_op_type.html#a3832456dc93125d7e63195ed55f57491", null ],
+    [ "WT_AOP_NONE", "enumcom_1_1wiredtiger_1_1db_1_1_async_op_type.html#ae0200a76371aa5eb98d1872c6a1d2bbc", null ],
+    [ "WT_AOP_REMOVE", "enumcom_1_1wiredtiger_1_1db_1_1_async_op_type.html#a08673ec19005842fa0b90af84a4f0304", null ],
+    [ "WT_AOP_SEARCH", "enumcom_1_1wiredtiger_1_1db_1_1_async_op_type.html#acf3bb35b2a382ca00d37d64e864827b9", null ]
+];
