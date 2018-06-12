67b82163875ad16ddc8cd862fb3ba47d3cfb4b6d
Michael Cahill <michael.cahill@mongodb.com>
2018-06-12 16:00:50 +1000

    Update auto-generated develop docs

--- develop/navtreedata.js
+++ develop/navtreedata.js
@@ -23,9 +23,9 @@ var NAVTREEINDEX =
 "classcom_1_1wiredtiger_1_1db_1_1_pack_input_stream.html",
 "durability.html",
 "group__wt.html#ga342eae1a0c6bbf8b7788f7e9fd5fc199",
-"group__wt.html#ga9ff1fba476748be20b404ed21a38a3e6",
-"license.html#license_leveldb",
-"struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a5b68b388279c64add7b0ef893b1d2e00"
+"group__wt.html#ga9f44ff6954d8f64698c73f6ff83517ff",
+"license.html#license_distribution",
+"struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a350e1db2cd51c8c2479b03beae9af90f"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- develop/navtreeindex5.js
+++ develop/navtreeindex5.js
@@ -1,5 +1,6 @@
 var NAVTREEINDEX5 =
 {
+"license.html#license_distribution":[0,10,4],
 "license.html#license_leveldb":[0,10,3],
 "license.html#license_library":[0,10,0],
 "lsm.html":[0,4,6],
@@ -248,6 +249,5 @@ var NAVTREEINDEX5 =
 "struct_w_t___f_i_l_e___h_a_n_d_l_e.html":[1,1,6],
 "struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a02b0e75636d323ce5bd42fdf7e83615e":[1,1,6,6],
 "struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a1e0b90f0a4fcf943e81b22b94069738b":[1,1,6,9],
-"struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a2479acb713a211b2d7cc84aaada4b29b":[1,1,6,3],
-"struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a350e1db2cd51c8c2479b03beae9af90f":[1,1,6,7]
+"struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a2479acb713a211b2d7cc84aaada4b29b":[1,1,6,3]
 };
