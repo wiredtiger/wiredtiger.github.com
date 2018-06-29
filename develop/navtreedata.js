60661a6cf517ae027eec97d0c69551bb92a6363a
Michael Cahill <michael.cahill@mongodb.com>
2018-06-29 18:00:20 +1000

    Update auto-generated develop docs

--- develop/navtreedata.js
+++ develop/navtreedata.js
@@ -23,9 +23,9 @@ var NAVTREEINDEX =
 "classcom_1_1wiredtiger_1_1db_1_1_pack_input_stream.html",
 "durability.html",
 "group__wt.html#ga33ac35e64c48896c09905f7b5e7068d4",
-"group__wt.html#ga9f2f8149523b1e6e4f64100ef198e050",
-"license.html#license_distribution",
-"struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a350e1db2cd51c8c2479b03beae9af90f"
+"group__wt.html#ga9f0f3fec551559d4bfef4175b4e2f629",
+"license.html#license_crc32-power8",
+"struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a1e0b90f0a4fcf943e81b22b94069738b"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
