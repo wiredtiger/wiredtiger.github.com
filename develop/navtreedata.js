00b782376dba0a8c7ad0ee06118f2fae47c7c377
Michael Cahill <michael.cahill@mongodb.com>
2018-07-18 05:00:16 +1000

    Update auto-generated develop docs

--- develop/navtreedata.js
+++ develop/navtreedata.js
@@ -22,7 +22,7 @@ var NAVTREEINDEX =
 ".html",
 "classcom_1_1wiredtiger_1_1db_1_1_pack_input_stream.html",
 "durability.html",
-"group__wt.html#ga33ac35e64c48896c09905f7b5e7068d4",
+"group__wt.html#ga339fdee8a1b559af384b994c0952c0ad",
 "group__wt.html#ga9f0f3fec551559d4bfef4175b4e2f629",
 "license.html#license_crc32-power8",
 "struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a1e0b90f0a4fcf943e81b22b94069738b"
