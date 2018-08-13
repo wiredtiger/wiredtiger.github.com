767c17ad536209b5d34188c3805d34e3a205a42e
Michael Cahill <michael.cahill@mongodb.com>
2018-08-14 02:00:07 +1000

    Update auto-generated develop docs

--- develop/navtreedata.js
+++ develop/navtreedata.js
@@ -21,11 +21,11 @@ var NAVTREEINDEX =
 [
 ".html",
 "classcom_1_1wiredtiger_1_1db_1_1_pack_input_stream.html",
-"durability.html",
-"group__wt.html#ga339fdee8a1b559af384b994c0952c0ad",
-"group__wt.html#ga9f0f3fec551559d4bfef4175b4e2f629",
-"license.html#license_crc32-power8",
-"struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a1e0b90f0a4fcf943e81b22b94069738b"
+"dump_formats.html#dump_formats_text",
+"group__wt.html#ga33883fdec5108a9e14dad7d6cec547e8",
+"group__wt.html#ga9efff35e3dd928596ac41c4200f7bb5c",
+"license.html",
+"struct_w_t___f_i_l_e___h_a_n_d_l_e.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- develop/navtreeindex5.js
+++ develop/navtreeindex5.js
@@ -1,5 +1,7 @@
 var NAVTREEINDEX5 =
 {
+"license.html":[0,10],
+"license.html":[7],
 "license.html#license_crc32-power8":[0,10,1],
 "license.html#license_crc32-zseries":[0,10,2],
 "license.html#license_distribution":[0,10,4],
@@ -247,7 +249,5 @@ var NAVTREEINDEX5 =
 "struct_w_t___e_x_t_r_a_c_t_o_r.html":[1,1,4],
 "struct_w_t___e_x_t_r_a_c_t_o_r.html#a4db101f00fce80af9ebd4f29f56f7b6a":[1,1,4,1],
 "struct_w_t___e_x_t_r_a_c_t_o_r.html#ad73a6c0903ad726e029ec25201ef12ae":[1,1,4,2],
-"struct_w_t___e_x_t_r_a_c_t_o_r.html#aead7ade6a5c1ea31917ed8cd0ef841aa":[1,1,4,0],
-"struct_w_t___f_i_l_e___h_a_n_d_l_e.html":[1,1,6],
-"struct_w_t___f_i_l_e___h_a_n_d_l_e.html#a02b0e75636d323ce5bd42fdf7e83615e":[1,1,6,6]
+"struct_w_t___e_x_t_r_a_c_t_o_r.html#aead7ade6a5c1ea31917ed8cd0ef841aa":[1,1,4,0]
 };
