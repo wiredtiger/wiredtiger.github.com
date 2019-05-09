6df3472241f2b2700ea07429da65d4f786089a3e
Luke Chen <luke.chen@mongodb.com>
2019-05-09 15:20:34 +1000

    Release 3.2.0

---
+++ 3.2.0/navtreedata.js
@@ -0,0 +1,33 @@
+var NAVTREE =
+[
+  [ "WiredTiger", "index.html", [
+    [ "Reference Guide", "index.html", "index" ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Class List", "annotated.html", "annotated" ],
+    [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+    [ "Class Members", "functions.html", [
+      [ "All", "functions.html", "functions_dup" ],
+      [ "Functions", "functions_func.html", "functions_func" ],
+      [ "Variables", "functions_vars.html", null ],
+      [ "Enumerator", "functions_eval.html", null ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ],
+    [ "Community", "community.html", null ],
+    [ "License", "license.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classcom_1_1wiredtiger_1_1db_1_1_pack_input_stream.html",
+"devdoc-schema.html#schema_create",
+"group__wt.html#ga2ed9313371aeee5334edb751df23992e",
+"group__wt.html#ga8ffe2e94e5c030e0a6f10e11c40c9da3",
+"group__wt.html#struct_w_t___i_t_e_m",
+"struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a13b8f3c923c8b0f8bb683ed3b192ee73",
+"wtperf.html#config"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ 3.2.0/navtreeindex7.js
@@ -0,0 +1,7 @@
+var NAVTREEINDEX7 =
+{
+"wtperf.html#config":[0,3,33,1],
+"wtperf.html#config":[0,4,27,1],
+"wtperf.html#monitor":[0,3,33,0],
+"wtperf.html#monitor":[0,4,27,0]
+};
