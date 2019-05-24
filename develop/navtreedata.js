ca34c7b196078eb7b5cc6b74b85ddfb02f88f01c
Michael Cahill <michael.cahill@mongodb.com>
2019-05-24 16:00:10 +1000

    Update auto-generated develop docs

--- develop/navtreedata.js
+++ develop/navtreedata.js
@@ -22,10 +22,10 @@ var NAVTREEINDEX =
 ".html",
 "classcom_1_1wiredtiger_1_1db_1_1_pack_input_stream.html",
 "devdoc-schema.html#schema_create",
-"group__wt.html#ga2ed9313371aeee5334edb751df23992e",
-"group__wt.html#ga8f6c39d714b2a99e809d6186cc0d4248",
-"group__wt.html#gga24152c6e1509c488192df653d511a82caa40ee85e0f72cadac54d2fb83ac75ded",
-"struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a0d3aae2ceac28f7b45188bcfa4306d45",
+"group__wt.html#ga2d67537b655e8bf2fd569a531b39c54b",
+"group__wt.html#ga8ecf265dba023ad13a6cdf3e02fa9d50",
+"group__wt.html#gga24152c6e1509c488192df653d511a82ca9847c3459bc2b0eaa527f91afdf84722",
+"struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a0aecfc732fe1a43d6b2ff3792a654c0f",
 "wtperf.html"
 ];
 
--- develop/navtreeindex7.js
+++ develop/navtreeindex7.js
@@ -1,8 +1,9 @@
 var NAVTREEINDEX7 =
 {
+"wtperf.html":[0,3,33],
 "wtperf.html":[0,4,27],
-"wtperf.html#config":[0,4,27,1],
 "wtperf.html#config":[0,3,33,1],
+"wtperf.html#config":[0,4,27,1],
 "wtperf.html#monitor":[0,4,27,0],
 "wtperf.html#monitor":[0,3,33,0]
 };
