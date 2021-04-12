309ea299f8d9120fd60fe125f9ccf6fbe8b182c4
Luke Chen <luke.chen@mongodb.com>
2021-04-12 13:28:45 +1000

    Release 10.0.0

---
+++ 10.0.0/navtreedata.js
@@ -0,0 +1,30 @@
+var NAVTREE =
+[
+  [ "WiredTiger", "index.html", [
+    [ "Reference Guide", "index.html", "index" ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Class List", "annotated.html", "annotated" ],
+    [ "Class Members", "functions.html", [
+      [ "All", "functions.html", null ],
+      [ "Functions", "functions_func.html", null ],
+      [ "Variables", "functions_vars.html", null ],
+      [ "Enumerator", "functions_eval.html", null ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ],
+    [ "Community", "community.html", null ],
+    [ "License", "license.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"admin.html",
+"ex_col_store_8c-example.html",
+"group__wt.html#ga476c96e900f1a66becc78adcd46c165b",
+"group__wt.html#ga944ea8324de96f55f426da509235cca1",
+"group__wt.html#gaf22dc79624dbedede3f8f173305246e6",
+"struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a7c47cf55646794f9e5cc3d185128ef3b"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
