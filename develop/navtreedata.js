227771b1444c4eb8bef82ce3c5c28ab5e96a0eed
svc-bot-doc-build <svc-wiredtiger-doc-build@10gen.com>
2021-05-01 04:23:16 +0000

    Update auto-generated develop docs

--- develop/navtreedata.js
+++ develop/navtreedata.js
@@ -22,8 +22,8 @@ var NAVTREEINDEX =
 "ex_col_store_8c-example.html",
 "group__wt.html#ga476c96e900f1a66becc78adcd46c165b",
 "group__wt.html#ga94e4c63cd5cc0a714ede12431a8e228c",
-"group__wt.html#gaf22dc79624dbedede3f8f173305246e6",
-"struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a6d85b1ce4631b7b4e09b735527b39b1e"
+"group__wt.html#gaf1f0bc4ced125c4993e8a76c649dddcf",
+"struct_w_t___e_x_t_e_n_s_i_o_n___a_p_i.html#a6d58298e356dbf58ac854c3d1af99678"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
